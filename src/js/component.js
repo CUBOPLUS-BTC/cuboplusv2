8d362f2d222e81255e7629b4c7ba35fac924edca
rodribp <rodrigobonilla177@gmail.com>
2024-08-04 16:00:18 -0600

    updating version content

--- src/js/component.js
+++ src/js/component.js
@@ -1,6 +1,6 @@
 var esButton = document.getElementsByClassName("es-button");
 var enButton = document.getElementsByClassName("en-button");
-const contentVersion = "1.9";
+const contentVersion = "2.0";
 const URL = "https://api-cuboplus.onrender.com/"
 
 function setElementsNavbarAndFooter(content) {
